fe20f70a44a5b76a732f347dec5909326cc1b7da
David Caldwell <ddcaldwell87@gmail.com>
2017-06-22 19:28:33 -0400

    started wishList object

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// the wish list object that manipulates data from the DOM
+var wishList = {
+  // the array that holds the list items
+  listItems: [],
+  // method to console log the entire list array
+  displayListItems: function() {
+    console.log(this.list);
+  },
+  // adds a new item to the list array
+  addListItems: function() {
+
+  },
+  // updates a single item from the list array
+  updateListItems: function(position, listItemText) {
+
+  },
+  // deletes a single item from the list array
+  deleteListItems: function(position) {
+
+  },
+  // toggles the completion status of a single list item in the list array
+  toggleCompleted: function(position) {
+
+  }
+};
