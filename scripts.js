52157e113dec5cfb58b5d4947f00cac8733cb575
David Caldwell <ddcaldwell87@gmail.com>
2017-06-22 19:44:08 -0400

    wishList object completed

--- scripts.js
+++ scripts.js
@@ -2,24 +2,35 @@
 var wishList = {
   // the array that holds the list items
   listItems: [],
-  // method to console log the entire list array
+  // console logs the entire list array
   displayListItems: function() {
-    console.log(this.list);
+    console.log(this.listItems);
   },
   // adds a new item to the list array
-  addListItems: function() {
-
+  addListItems: function(listItemText) {
+    this.listItems.push({
+      listItemText: listItemText,
+      completed: false
+    });
+    this.displayListItems();
   },
   // updates a single item from the list array
   updateListItems: function(position, listItemText) {
-
+    this.listItems[position] = listItemText;
+    this.displayListItems();
   },
   // deletes a single item from the list array
   deleteListItems: function(position) {
-
+    this.listItems.splice(position, 1);
+    this.displayListItems();
   },
   // toggles the completion status of a single list item in the list array
   toggleCompleted: function(position) {
-
+    if (this.listItems[position].completed === false) {
+      this.listItems[position].completed = true;
+    } else {
+      this.listItems[position].completed = false;
+    }
+    this.displayListItems();
   }
 };
